e442c72efe8fef6df0c0d1cb721d3af5b64bd847
kawser2917 <kawserahmed8492@gmail.com>
2023-11-28 00:13:46 +0600

    navigation menu started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+// import Daisynav from './components/DaisyNav/Daisynav'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
+    {/* <Daisynav></Daisynav> */}
+    <Navbar></Navbar>
       <h1 className='text-7xl bg-green-400'>Vite + React</h1>
     </>
   )
